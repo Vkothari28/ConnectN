93ff2482bab61a1f9af981047223484439083fca
Vinit <vinitkothari28@gmail.com>
2020-02-12 15:44:59 -0500

    Merge branch 'master' of https://github.com/jerryperez25/ConnectN
     Conflicts:
    	alpha_beta_agent.py

--- alpha_beta_agent.py
+++ alpha_beta_agent.py
@@ -1,6 +1,7 @@
 import math
 import agent
 
+
 ###########################
 # Alpha-Beta Search Agent #
 ###########################
@@ -17,30 +18,36 @@ class AlphaBetaAgent(agent.Agent):
         # Max search depth
         self.max_depth = max_depth
 
-    def path_heuristics(self,brd,player):
+    def path_heuristics(self, brd, player):
 
-        if player==1:
-            user=1
-            opponent=2
+        if player == 1:
+            user = 1
+            opponent = 2
 
         else:
-             user=2
-             opponent=1
+            user = 2
+            opponent = 1
 
-            #using board.getoutcome() to determine a winning or a losing board
-       if brd.get_outcome() == user and brd.player==opponent:
+        # using board.getoutcome() to determine a winning or a losing board
+
+    if brd.get_outcome() == user and brd.player == opponent:
         return 100000000000000000000000000000000000000000000000
 
-       if brd.get_outcome()==opponent and brd.player== user:
+    if brd.get_outcome() == opponent and brd.player == user:
         return -9999999999999999999999999999999999999999999999999
 
-
-     for i in range (0,board.h):
-         for j in range(0,board.w):
-             if brd.board[i][j]!=0:
-                 if brd.is_any_line_at(i, j):
-                     if brd.board[i][j] == opponen
-
+    board_heuristics = 0
+    for i in range(0, board.h):
+        for j in range(0, board.w):
+            if brd.board[i][j] != 0:
+                if brd.is_any_line_at(i, j):
+                    if brd.board[i][j] == opponent:
+                        board_heuristics -= 10
+                    elif brd.board[i][j] == user:
+                        board_heuristics += 1
+                    else:
+                        board_heuristics += 0
+                    return board_heuristics
 
     # Pick a column.
     #
@@ -75,5 +82,5 @@ class AlphaBetaAgent(agent.Agent):
             # (This internally changes nb.player, check the method definition!)
             nb.add_token(col)
             # Add board to list of successors
-            succ.append((nb,col))
+            succ.append((nb, col))
         return succ
