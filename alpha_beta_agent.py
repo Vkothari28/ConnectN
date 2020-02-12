6056dda35589c0dadb20a7ca9f509efaa25c6992
Vinit <vinitkothari28@gmail.com>
2020-02-11 22:55:36 -0500

    some changes made a path heuristics thing

--- alpha_beta_agent.py
+++ alpha_beta_agent.py
@@ -17,6 +17,38 @@ class AlphaBetaAgent(agent.Agent):
         # Max search depth
         self.max_depth = max_depth
 
+    def path_heuristics(self,brd,player):
+
+        if player==1:
+            user=1
+            opponent=2
+
+        else:
+             user=2
+             opponent=1
+
+            #using board.getoutcome() to determine a winning or a losing board
+       if brd.get_outcome() == user and brd.player==opponent:
+        return 100000000000000000000000000000000000000000000000
+
+       if brd.get_outcome()==opponent and brd.player== user:
+          return -9999999999999999999999999999999999999999999999999
+
+    board_heuristics =0
+    for i in range (0,board.h):
+         for j in range(0,board.w):
+             if brd.board[i][j]!=0:
+                 if brd.is_any_line_at(i, j):
+                     if brd.board[i][j] == opponent:
+                         board_heuristics -= 10
+                     elif brd.board[i][j]== user:
+                         board_heuristics +=1
+                     else:
+                         board_heuristics += 0
+                        return board_heuristics
+
+
+
     # Pick a column.
     #
     # PARAM [board.Board] brd: the current board state
