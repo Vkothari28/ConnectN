b84ab69baf8482c1c931518b61596617616f8c80
isaacbee <isaac_beeman@hotmail.com>
2020-02-13 01:19:38 -0500

    full alpha-beta pruning implementation :)

--- alpha_beta_agent.py
+++ alpha_beta_agent.py
@@ -17,31 +17,36 @@ class AlphaBetaAgent(agent.Agent):
         super().__init__(name)
 
         self.max_depth = max_depth
-        self.player = None
+        self.me = None
+        self.you = None
 
-    # ISAAC: probably shouldn't focus on this until we have a working 
-    #        minimax algorithm. I adjusted the stupid large heuristic values 
+    # ISAAC: we should add a heuristic here that gives/removes value states
+    #        that have more in a row
     def heuristic(self, brd):
+        """Assign a heuristic value to the current state of the board"""
 
-        # using board.getoutcome() to determine a winning or a losing board
-        if brd.get_outcome() == self.player and brd.player != self.player:
-            return 1000000
+        if "defensive": 
+            # This simply causes the ai to play defensively:
+            #   if the AI can win, play that move
+            #   else if the other player is about to win, block it
+            if brd.get_outcome() == self.me and brd.player == self.you:
+                return math.inf
 
-        if brd.get_outcome() != self.player and brd.player == self.player:
-            return -1000000
+            if brd.get_outcome() == self.you and brd.player == self.me:
+                return -math.inf
 
-        board_heuristics = 0
-        for i in range(0, brd.h):
-            for j in range(0, brd.w):
-                if brd.board[i][j] != 0:
-                    if brd.is_any_line_at(i, j):
-                        if brd.board[i][j] != self.player:
-                            board_heuristics -= 10
-                        elif brd.board[i][j] == self.player:
-                            board_heuristics += 1
-                        else:
-                            board_heuristics += 0
-        return board_heuristics
+            value = 0
+            for x in range(0, brd.w):
+                for y in range(0, brd.h):
+                    if brd.board[y][x] != 0:
+                        if brd.is_any_line_at(x, y):
+                            if brd.board[y][x] == self.you:
+                                value -= 10
+                            elif brd.board[y][x] == self.me:
+                                value += 1
+                            else:
+                                value += 0
+        return value
 
     # Pick a column.
     #
@@ -52,7 +57,52 @@ class AlphaBetaAgent(agent.Agent):
     def go(self, brd):
         """Search for the best move (choice of column for the token)"""
         # Your code here
+        self.me = brd.player
+        self.you = (not (brd.player-1))+1
+        alpha = -math.inf
+        beta = math.inf
+        ((b,c),v) = self.max(brd, self.max_depth, alpha, beta)
+        return c
+    
+    def max(self, brd, n, alpha, beta):
+        if n == 0:
+            return ((brd, None), self.heuristic(brd))
         
+        best_value = -math.inf
+        best_brd = None
+        best_col = None
+
+        successors = self.get_successors(brd)
+        for (board, column) in successors:
+            ((b, c), value) = self.min(board, n-1, alpha, beta)
+            if value > best_value:
+                best_brd = board
+                best_col = column
+                best_value = value
+            if best_value >= beta:
+                return ((best_brd, best_col), best_value)
+            alpha = max(alpha, best_value)
+        return ((best_brd, best_col), best_value)
+
+    def min(self, brd, n, alpha, beta):
+        if n == 0:
+            return ((brd, None), self.heuristic(brd))
+        
+        best_value = math.inf
+        best_brd = None
+        best_col = None
+
+        successors = self.get_successors(brd)
+        for (board, column) in successors:
+            ((b, c), value) = self.max(board, n-1, alpha, beta)
+            if value < best_value:
+                best_brd = board
+                best_col = column
+                best_value = value
+            if best_value <= alpha:
+                return ((best_brd, best_col), best_value)
+            beta = min(beta, best_value)
+        return ((best_brd, best_col), best_value)
 
     # Get the successors of the given board.
     #
